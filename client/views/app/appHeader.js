3c4941d6b139511800a20c8db9371cf2e390e1e3
Anders Ramsay <andersr@gmail.com>
2015-08-17 14:30:29 -0400

    display empty login modal

--- client/views/app/appHeader.js
+++ client/views/app/appHeader.js
@@ -1,22 +1,24 @@
 Template.appHeader.events({
   'click .create-post-from-local': function(){
+
+    AntiModals.overlay('loginModal');
     //console.log("Title: #{Session.get 'docTitle'}, Content: #{Session.get 'slideContent'}")
 
-    var postAttributes = {
-      title: Session.get("docTitle"),
-      content: Session.get("slideContent")
-    }
+    // var postAttributes = {
+    //   title: Session.get("docTitle"),
+    //   content: Session.get("slideContent")
+    // }
 
-    Meteor.call('createPost', postAttributes,
-      function(error, result){
-        if (error) {
-          alert(error.reason);
-        } else{
-          Session.setPersistent("slideContent", "");
-           console.log("New post: " + result._id)
-        };
+    // Meteor.call('createPost', postAttributes,
+    //   function(error, result){
+    //     if (error) {
+    //       alert(error.reason);
+    //     } else{
+    //       Session.setPersistent("slideContent", "");
+    //        console.log("New post: " + result._id)
+    //     };
 
-    });
+    // });
   }
 
 });

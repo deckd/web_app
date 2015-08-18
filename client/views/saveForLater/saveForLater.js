93f8fd699fbf58550f9aacdf26143435d355c8c9
Anders Ramsay <andersr@gmail.com>
2015-08-18 16:13:12 -0400

    display user nav

--- client/views/saveForLater/saveForLater.js
+++
@@ -1,9 +0,0 @@
-Template.saveForLater.onCreated(function(){
-  AntiModals.overlay('loginModal');
-});
-
-Template.saveForLater.onRendered(function(){
-   if(AntiModals.dismissOverlay('loginModal')){
-     Router.go('home');
-   };
-});

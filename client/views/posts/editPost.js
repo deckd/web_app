43e8adb821ec7316310a368ec6f551a335f902e1
Anders Ramsay <andersr@gmail.com>
2015-08-20 16:00:25 -0400

    prep for slide <> posts cleanup

--- client/views/posts/editPost.js
+++ client/views/posts/editPost.js
@@ -1,3 +1,11 @@
 Template.editPost.onRendered(function(){
   $('#post_content').focus();
 });
+
+Template.editPost.helpers({
+  postContent: function(){
+    var post = Posts.findOne({_id: this._id});
+
+    return post.content;
+  }
+});
--- client/views/users/loginModal.js
+++ client/views/users/loginModal.js
@@ -1,22 +1,4 @@
 Template.loginModal.onDestroyed(function(){
-
   //here is where I can handle dismissing of the login modal
   console.log('the loginmodal was removed');
-
 });
-
-// Template.loginModal.helpers({
-//   saveForLater: function(){
-   
-//     // if (Session.get("loginModal")) {} else{};
-//   }
-
-// });
-
-// Template.body.helpers({
-//   "click .anti-modal-overlay":function(e){
-//     // e.stopPropagation();
-//     console.log("overlay closed");
-//   }
-
-// });

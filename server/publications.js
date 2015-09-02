f89a76185d82693525959bfe48a1ce24a2a36c54
Anders Ramsay <andersr@gmail.com>
2015-09-02 16:15:21 -0400

    remove duplicate posts publish

--- server/publications.js
+++ server/publications.js
@@ -4,3 +4,27 @@ Meteor.publish('posts', function() {
 // Meteor.publish('votes', function() {
 //   return Votes.find(); 
 // });
+
+// TODO: only publish this author's posts:
+
+// Meteor.publish("posts", function(){
+//   var currentUserId = this.userId;  
+//   return Posts.find({authorId: currentUserId });
+// });
+
+// TODO: Only publish tags for this author:
+/// return Tags.find({authors: Meteor.userId() }); 
+
+// Meteor.publish("userData", function () {
+//   if (this.userId) {
+//     return Meteor.users.find( {_id: this.userId},
+//       {fields: 
+//         {
+//           "services.google.picture": true,
+//           "services.google.name": true
+//         }
+//       });
+//   } else {
+//     this.ready();
+//   }
+// });
